12341867621546fd1aaa5aa099835450c355ae80
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-01-03 12:21:45 -0600

    updated

---
+++ src/StringExercise.java
@@ -0,0 +1,32 @@
+public class StringExercise {
+  public static void main(String[] args) {
+
+    // For each of the following output examples: create a String variable that
+    // contains the desired output and print it out to the console, you can do this
+    // with only one String variable and one print statement for each output
+    // example.
+
+    // We don't need no education
+    // We don't need no thought control
+
+    // Check "this" out!, "s inside of "s!
+
+    // In windows, the main drive is usually C:\
+
+    // I can do backslashes \, double backslashes \\,
+    // and the amazing triple backslash \\\!
+
+    String noEducation = "We don't need no education";
+    String noThoughtControl = "We don't need no thought control";
+    String checkThisOut = "Check \"this\" out!, \"s inside of \"s!";
+    String windows = "In windows, the main drive is usually C:\\";
+    String backslashes = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+
+    System.out.println(noEducation);
+    System.out.println(noThoughtControl);
+    System.out.println(checkThisOut);
+    System.out.println(windows);
+    System.out.println(backslashes);
+
+  }
+}
---
+++ src/bob.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class bob {
+  public static void main(String[] args) {
+
+    // Bob is a lackadaisical teenager. In conversation, his responses are very
+    // limited.
+
+    // Bob answers 'Sure.' if you ask him a question. (the input ends with a
+    // question mark)
+    
+    // He answers 'Whoa, chill out!' if you yell at him. (the input ends with an
+    // exclamation mark)
+    
+    // He says 'Fine. Be that way!' if you address him without actually saying
+    // anything. (empty input)
+    
+    // He answers 'Whatever.' to anything else.
+
+    // Write the Java code necessary so that a user of your command line application
+    // can have a conversation with Bob.
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Say something to Bob: ");
+    String userInput = scanner.nextLine();
+    if (userInput.endsWith("?")) {
+      System.out.println("Sure.");
+    } else if (userInput.endsWith("!")) {
+      System.out.println("Whoa, chill out!");
+    } else if (userInput.isEmpty()) {
+      System.out.println("Fine. Be that way!");
+    } else {
+      System.out.println("Whatever.");
+    }
+
+  }
+}
